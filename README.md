9a3deed4e80ecf104e3eb2098ffa733937f0a318
Pedro Botica <pedrobotica@MBP-de-Pedro-4.home>
2024-03-28 20:38:58 +0000

    Stock stats now come in Yu-Gi-Oh! card format

---
+++ README.md
@@ -0,0 +1,4 @@
+Project created to learn how to use APIs, to get it to work you need an API key from https://site.financialmodelingprep.com/
+
+
+
--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const app = express();
 const API_URL = "https://financialmodelingprep.com/api/v3";
 const API_KEY = `${process.env.STOCK_API_KEY}`
 
+function shortenToFirstSentence(text) {
+    
+    const sentenceEndRegex = /(?<!\b[a-z])(?<!Inc)(?<!Ltd)[\.\?!](\s|$)/;
+    const match = sentenceEndRegex.exec(text);
+    if (match) {
+      return text.substring(0, match.index + 1);
+    }
+    return text;
+}
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
@@ -19,19 +29,22 @@ app.get("/", (req, res) =>{
 
 app.post("/submit", async (req, res) => {
     const stockToCheck = req.body.stock
-    console.log(stockToCheck)
     try {
         const response = await axios.get(API_URL + "/profile/"+ stockToCheck + API_KEY);
         const result = response.data
+        console.log(result[0])
         const name = result[0].companyName
         const image = result[0].image
         const price = result[0].price
         const ticker = result[0].symbol
         const change = result[0].changes
-        const description = result[0].description
+        const mktCap = result[0].mktCap
+        const sector = result[0].sector
+        const sectorCap = sector.toUpperCase()
+        const description = shortenToFirstSentence(result[0].description)
         console.log(name)
         res.render("index.ejs", {
-           name: name, image: image, price: price, ticker: ticker, change : change, description: description
+           name: name, image: image, price: price, ticker: ticker, change : change, description: description, mktCap: mktCap, sector: sectorCap
         })
     } catch(error){
         console.log(error.message)
